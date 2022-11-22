25bea54645e3acf25706c2a9d81927d773bfeb07
Nahuel Castro <68122322+nauee@users.noreply.github.com>
2022-11-22 10:38:18 -0300

    Ejecucion por partes

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,20 @@ use coffeewards::cafeteria::Cafeteria;
 use coffeewards::constantes::CANT_CAFETERIAS;
 
 fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
     let mut node_threads = vec![];
 
-    for id in 0..CANT_CAFETERIAS {
+    if args.len() > 1 {
+        let id = args[1].parse::<usize>().unwrap();
+        if id >= CANT_CAFETERIAS {
+            eprintln!("[ERROR] El id del nodo debe ser menor a la cantidad de nodos");
+            return;
+        }
         node_threads.push(
             thread::Builder::new()
                 .name(format!("Cafeteria {}", id))
-                .spawn(
-                    move || match Cafeteria::new(id, format!("./pedidos/pedidos{}.txt", id)) {
+                .spawn(move || {
+                    match Cafeteria::new(id, format!("./pedidos/pedidos{}.txt", id)) {
                         Ok(mut cafeteria) => {
                             if let Err(msg) = cafeteria.run() {
                                 println!("[ERROR] nodo {}: {}", id, msg);
@@ -20,10 +26,30 @@ fn main() {
                         Err(e) => {
                             eprintln!("[ERROR] no se pudo crear la cafeteria {}: {}", id, e);
                         }
-                    },
-                )
+                    }
+                })
                 .unwrap(),
         );
+    } else {
+        for id in 0..CANT_CAFETERIAS {
+            node_threads.push(
+                thread::Builder::new()
+                    .name(format!("Cafeteria {}", id))
+                    .spawn(move || {
+                        match Cafeteria::new(id, format!("./pedidos/pedidos{}.txt", id)) {
+                            Ok(mut cafeteria) => {
+                                if let Err(msg) = cafeteria.run() {
+                                    println!("[ERROR] nodo {}: {}", id, msg);
+                                }
+                            }
+                            Err(e) => {
+                                eprintln!("[ERROR] no se pudo crear la cafeteria {}: {}", id, e);
+                            }
+                        }
+                    })
+                    .unwrap(),
+            );
+        }
     }
 
     for handle in node_threads {
